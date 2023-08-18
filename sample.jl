67417d1bb5b8b3ea0e9f5452205731b5437c5d75
rc-balaji <balajic@drngpit.ac.in>
2023-08-18 16:16:25 +0530

    new

--- sample.jl
+++ sample.jl
@@ -1,1 +1,57 @@
-print("Jai")
+
+
+println(2-45im)
+
+
+println(real(2-45im))
+println(imag(2-45im))
+println(conj(2-45im))
+
+
+println(2/3)
+
+
+println(~10)
+println()
+println()
+println()
+println()
+# println()
+# println()
+# println()
+println()
+
+# for i in 0:10
+#     for j in 0:10
+#         if(i>=j)
+#             print("* ") 
+#         end
+    
+#     end
+#     println()
+# end
+
+name1="Hai, "
+name2="Ram"
+println(name1,name2)
+
+
+println("1 + 2 = $(1+2)")
+
+
+
+
+
+v=[1,2,3,4,5]
+# print(str[2:4])
+println("v=$v")
+
+
+st="gjashdgwew"
+
+println(findlast("gjash",st))
+println(findfirst('g',st))
+println(findlast('0',st))
+
+
+print()
